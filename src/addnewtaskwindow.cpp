09de80b0bd5ab7c5639a3a34562b18beac964c7a
Nikita Lebiodkin <74015761+trollonos@users.noreply.github.com>
2021-05-18 22:27:08 +0300

    update if-else branches

--- src/addnewtaskwindow.cpp
+++ src/addnewtaskwindow.cpp
@@ -12,10 +12,6 @@ addnewtaskwindow::addnewtaskwindow(QWidget *parent,MainWindow *window) :
 
     ui->setupUi(this);
     this->window = window;
-
-
-
-
 }
 
 addnewtaskwindow::~addnewtaskwindow()
@@ -27,66 +23,72 @@ void addnewtaskwindow::on_pushButton_clicked()
 {
     QMessageBox msgBox;
     int ret=msgBox.warning(this,tr("Предупреждение"),tr("Сохранить изменения?"),QMessageBox::Ok,QMessageBox::Cancel);
-     if(ret==QMessageBox::Ok){
-    QString _taskname=ui->_tasktitlearea->toPlainText()
-            ,_taskdetails=ui->_taskdetailsarea->toPlainText();
+    if(ret==QMessageBox::Ok)
+    {
+        QString _taskname=ui->_tasktitlearea->toPlainText()
+               ,_taskdetails=ui->_taskdetailsarea->toPlainText();
+        QString _taskpriority=ui->_taskpriorityarea->toPlainText();
+        int day,month,year;
 
-    QString _taskpriority=ui->_taskpriorityarea->toPlainText();
-    int day,month,year;
+        take_time(day,month,year);
 
-    take_time(day,month,year);
-
-    QSqlQuery query;
-    bool today_tasks=false,overdue_tasks=false,task_data_table=false;
-    if(!window->is_date_pass(day,month,year)){
-        if(_current_day==day&&_current_month==month&&_current_year==year){
-            query.prepare("INSERT INTO today_tasks (taskname, taskdetails, priority,id,day,month,year) "
+        QSqlQuery query;
+        bool today_tasks=false,overdue_tasks=false,task_data_table=false;
+        if(!window->is_date_pass(day,month,year))
+        {
+            if(_current_day==day&&_current_month==month&&_current_year==year)
+            {
+                query.prepare("INSERT INTO today_tasks (taskname, taskdetails, priority,id,day,month,year) "
                           "VALUES (:taskname, :taskdetails, :priority,:id,:day,:month,:year)");
-            today_tasks=true;
-
-        }else{
-
-
-
-
-        query.prepare("INSERT INTO task_data_table (taskname, taskdetails, priority,id,day,month,year) "
-                      "VALUES (:taskname, :taskdetails, :priority,:id,:day,:month,:year)");
-        task_data_table=true;
-        }
-    }else{
-        query.prepare("INSERT INTO overdue_tasks (taskname, taskdetails, priority,id,day,month,year) "
-                      "VALUES (:taskname, :taskdetails, :priority,:id,:day,:month,:year)");
-    overdue_tasks=true;
-    }
-    int priority;
-    if(_string_or_not(_taskpriority)&&window->is_string_valid(_taskname)){
-        qDebug()<<"is in";
-        priority=_convert_to_int(_taskpriority);
-        query.bindValue(":priority",priority);
-        //window->giveid(id);
-        int max=window->_giveid();
-
-        query.bindValue(":taskname",_taskname);
-        query.bindValue(":taskdetails",_taskdetails);
-        query.bindValue(":id",max);
-        query.bindValue(":day",day);
-        query.bindValue(":month",month);
-        query.bindValue(":year",year);
-        query.exec();
-        window->_assign_checkbox_id(max);
-        window->addtask(_taskname,_taskdetails,priority,max,day,month,year);
-
-
-    }else{
-        if(!_string_or_not(_taskpriority)){
-        QMessageBox *mb=new QMessageBox;
-        mb->warning(this,tr("Предупреждение"),tr("Неверное значение приоритетности, введите верное значение"),QMessageBox::Ok);
-        }else if(!window->is_string_valid(_taskname)){
-            QMessageBox *mb=new QMessageBox;
-            mb->warning(this,tr("Предупреждение"),tr("Недопустимое название, введите другое"),QMessageBox::Ok);
-
+                today_tasks=true;
             }
+            else
+            {
+                query.prepare("INSERT INTO task_data_table (taskname, taskdetails, priority,id,day,month,year) "
+                      "VALUES (:taskname, :taskdetails, :priority,:id,:day,:month,:year)");
+                task_data_table=true;
+            }
+        }
+        else
+        {
+            query.prepare("INSERT INTO overdue_tasks (taskname, taskdetails, priority,id,day,month,year) "
+                      "VALUES (:taskname, :taskdetails, :priority,:id,:day,:month,:year)");
+            overdue_tasks=true;
+        }
+        int priority;
+        if(_string_or_not(_taskpriority)&&window->is_string_valid(_taskname))
+        {
+            qDebug()<<"is in";
+            priority=_convert_to_int(_taskpriority);
+            query.bindValue(":priority",priority);
+            //window->giveid(id);
+            int max=window->_giveid();
 
+            query.bindValue(":taskname",_taskname);
+            query.bindValue(":taskdetails",_taskdetails);
+            query.bindValue(":id",max);
+            query.bindValue(":day",day);
+            query.bindValue(":month",month);
+            query.bindValue(":year",year);
+            query.exec();
+            window->_assign_checkbox_id(max);
+            window->addtask(_taskname,_taskdetails,priority,max,day,month,year);
+        }
+        else
+        {
+            if(!_string_or_not(_taskpriority))
+            {
+                QMessageBox *mb=new QMessageBox;
+                mb->warning(this,tr("Предупреждение"),tr("Неверное значение приоритетности, введите верное значение"),QMessageBox::Ok);
+            }
+            else
+            {
+                if(!window->is_string_valid(_taskname))
+                {
+                    QMessageBox *mb=new QMessageBox;
+                    mb->warning(this,tr("Предупреждение"),tr("Недопустимое название, введите другое"),QMessageBox::Ok);
+                }
+            }
         }
      }
 
